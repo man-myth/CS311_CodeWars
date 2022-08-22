371868ec17d72e96db02590cf93acf6a5c9ba7ea
Manmeet <manmeets1100@gmail.com>
2022-08-22 15:48:27 +0800

    Add StringSplit.java

--- src/StringSplit.java
+++ src/StringSplit.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Complete the solution so that it splits the string into pairs of two characters.
+ * If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+ */
 public class StringSplit {
     public static String[] solution(String s) {
         ArrayList<String> res = new ArrayList<>();
