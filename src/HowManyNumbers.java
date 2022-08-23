1df754d4b69cde308756d5ac142be6c287559a17
man-myth <manmeets1100@gmail.com>
2022-08-23 21:54:38 +0800

    add HowManyNumbers.java

---
+++ src/HowManyNumbers.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HowManyNumbers {
+
+    static Long count = 0L;
+    static Long min = 0L;
+    static Long max = 0L;
+    public static List<Long> findAll(final int sumDigits, final int numDigits) {
+        // Your code here!!
+        List<Long> res = new ArrayList<>();
+
+
+        getNumbers(0L, 1, sumDigits, numDigits);
+
+        if (count > 0) {
+            res.add(count);
+            res.add(min);
+            res.add(max);
+        }
+        return res;
+
+    }
+    private static void getNumbers(Long currNum, int prevNum, int sumLeft, int digitsLeft) {
+        if (sumLeft == 0 && digitsLeft == 0) {
+            if (count == 0) min = currNum;
+            min = min < currNum ? min : currNum;
+            max = max > currNum ? max : currNum;
+            count++;
+        } else if (digitsLeft != 0) {
+            for (int i = prevNum; i < 10; i++) {
+                getNumbers(10 * currNum + i, i,sumLeft - i, digitsLeft - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Long> ls= findAll(10,3);
+        for (Long l: ls) {
+            System.out.println(l);
+        }
+    }
+}
