857dc004d9ab6f1b3d722253d1de7cdd8369d8dd
man-myth <manmeets1100@gmail.com>
2022-08-30 11:32:21 +0800

    fix HowManyNumbers

--- src/HowManyNumbers.java
+++ src/HowManyNumbers.java
@@ -1,50 +1,54 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.*;
 
 public class HowManyNumbers {
-
-    static Long count = 0L;
-    static Long min = 0L;
-    static Long max = 0L;
     public static List<Long> findAll(int sumDigits, int numDigits) {
-        // Your code here!!
-        List<Long> res = new ArrayList<>();
-        getNumbers(0L, 1, sumDigits, numDigits);
-        if (count > 0) {
-            res.add(count);
+        List<long[]> numbers = new ArrayList<>();  // list of numbers in array ex.[[1,2,3]]
+        List<Long> allNumbers = new ArrayList<>(); // list of numbers ex: [123]
+        List<Long> res = new ArrayList<>();        // consists [count, min, max]
+        for(long[] x: digits(numDigits, 1L)){
+            if (LongStream.of(x).sum() == sumDigits){
+                numbers.add(x);
+            }
+        }
+        if (numbers.size() != 0){
+            for (long[] l : numbers){
+                Long L = 0L;
+                for (int i = 0; i <l.length; i++){
+                    L = L*10 + l[i];
+                }
+                allNumbers.add(L);
+            }
+
+            Long max = allNumbers.get(allNumbers.size()-1);
+            Long min = allNumbers.get(0);
+            res.add((long) allNumbers.size());
             res.add(min);
             res.add(max);
         }
         return res;
     }
-    private static void getNumbers(Long currNum, int prevNum, int sumLeft, int digitsLeft) {
-        if (sumLeft == 0 && digitsLeft == 0) {
-            if (count == 0) {
-                min = currNum;
-            }
-            if(min < currNum){
-                min = min;
-            } else{
-                min = currNum;
-            }
-            max = max > currNum ? max : currNum;
-            if(min < currNum){
-                min = min;
-            } else{
-                min = currNum;
-            }
-            count++;
-        } else if (digitsLeft != 0) {
-            for (int i = prevNum; i < 10; i++) {
-                getNumbers(10 * currNum + i, i,sumLeft - i, digitsLeft - 1);
-            }
-        }
-    }
 
-    public static void main(String[] args) {
-        List<Long> ls= findAll(84,4);
-        for (Long l: ls) {
-            System.out.println(l);
+    private static List<long[]> digits(int d, long start){
+        List<long[]> digs = new ArrayList<>();
+        if(d == 1){
+            for (long i = start; i <10; i++){
+                digs.add(new long[]{i});
+            }
+        } else{
+            for (long i = start; i <10; i++){
+                for (long[] j: digits(d-1, i)){
+                    long[] nums = new long[d]; // [1] + [2,3] = [1,2,3]
+                    nums[0] = i;
+                    for (int t = 0; t<j.length; t++){
+                        nums[t+1] = j[t];
+                    }
+                    digs.add(nums);
+                }
+            }
         }
+        return digs;
     }
+    
 }
